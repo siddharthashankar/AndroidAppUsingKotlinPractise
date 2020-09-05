769e7fc4fa9e6e12cfdaa91194e61e76ee5327d4
SiddharthaShankar84 <shankar.siddhartha84@gmail.com>
2020-09-05 17:19:18 +0800

    FCM Implementation: LocalBroadcastManager use for background notification.

--- app/src/main/java/com/sid/androidusingkotlin/fcm/FCMActivity.kt
+++ app/src/main/java/com/sid/androidusingkotlin/fcm/FCMActivity.kt
@@ -1,12 +1,50 @@
 package com.sid.androidusingkotlin.fcm
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
+import androidx.appcompat.app.AlertDialog
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.sid.androidusingkotlin.R
+import com.sid.androidusingkotlin.fcm.MyFirebaseMessagingService.Companion.INTENT_ACTION_SEND_MESSAGE
+import java.util.*
 
 class FCMActivity : AppCompatActivity() {
+
+    lateinit var receiver: BroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fcm)
+
+        receiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val fcmMsg = intent?.getStringExtra("message")
+
+                AlertDialog
+                    .Builder(this@FCMActivity)
+                    .setMessage(fcmMsg)
+                    .setTitle("UOB FCM Title")
+                    .setPositiveButton("Ok"){_ , _ -> }
+                    .create().show()
+            }
+
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter(INTENT_ACTION_SEND_MESSAGE)
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
+    }
+
+
 }
--- app/src/main/java/com/sid/androidusingkotlin/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/sid/androidusingkotlin/fcm/MyFirebaseMessagingService.kt
@@ -2,6 +2,7 @@ package com.sid.androidusingkotlin.fcm
 
 import android.content.Intent
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.firebase.installations.remote.TokenResult
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -20,14 +21,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         Log.d(TAG, "FCM--"+remoteMessage.notification?.title+", "+remoteMessage.notification?.body)
 
-        val intent = Intent(this@MyFirebaseMessagingService, HomeActivity::class.java)
+        // Way1 :  direct pass FCM msg to Activity
+/*        val intent = Intent(this@MyFirebaseMessagingService, HomeActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.putExtra("fcm_title", remoteMessage.notification?.title)
         intent.putExtra("fcm_msg", remoteMessage.notification?.body)
-        startActivity(intent)
+        startActivity(intent)*/
+
+        // Way2:  Pass FCM msg to LocalBroadcastManager
+        passMsgToActivity(remoteMessage.notification?.body)
     }
 
+    private fun passMsgToActivity(fcmMsg: String?) {
+        val intent = Intent().apply {
+            action = INTENT_ACTION_SEND_MESSAGE
+            putExtra("message", fcmMsg)
+        }
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
 
-
+    companion object{
+        const val INTENT_ACTION_SEND_MESSAGE = "INTENT_ACTION_SEND_MESSAGE"
+    }
 
 }
